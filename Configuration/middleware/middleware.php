45597b445904ce2f3bc19e8726568a0f106a5fad
aosardez <aosardez@up.edu.ph>
2017-05-02 13:35:22 +0800

    Added middleware for removing trailing "/".
    Added middleware for removing trailing "/".

--- Configuration/middleware/middleware.php
+++ Configuration/middleware/middleware.php
@@ -1,6 +1,28 @@
 <?php
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 $container = $app->getContainer();
  
 if ($container->get("settings")["displayErrorDetails"]) {
     $app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
 };
+
+$app->add(function (RequestInterface $request, ResponseInterface $response, callable $next) {
+    $uri = $request->getUri();
+    $path = $uri->getPath();
+    if ($path != '/' && substr($path, -1) == '/') {
+        // permanently redirect paths with a trailing slash
+        // to their non-trailing counterpart
+        $uri = $uri->withPath(substr($path, 0, -1));
+        
+        if($request->getMethod() == 'GET') {
+            return $response->withRedirect((string)$uri, 301);
+        }
+        else {
+            return $next($request->withUri($uri), $response);
+        }
+    }
+
+    return $next($request, $response);
+});
